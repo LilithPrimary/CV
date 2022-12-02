10bf412fd015100502843be5fe5f51887df17cc0
Denna <lilithprimary@gmail.com>
2022-12-02 09:44:30 +0100

    feat: new project

--- assets/script/projects.js
+++ assets/script/projects.js
@@ -1,117 +1,142 @@
-
 const projects = [
+  {
+    name: 'Project management app',
+    img: './assets/svg/pma.svg',
+    url: 'https://gleeful-arithmetic-daffb3.netlify.app/',
+    gitUrl:
+      'https://github.com/RSS-React-2022Q3-Team-27/project-management-app',
+    description:
+      'Project management app. Team work. Graduation project for React course. Used React, Typescript, React-redux, RTK, RTK Query, react-router-dom, axios, jwt-decode, React-Toastify, React-i18next, React-hook-form, @hello-pangea/dnd (react-beautiful-dnd fork), Material UI, CRA',
+  },
   {
     name: 'RS Lang',
     img: './assets/svg/rslang.svg',
     url: 'https://rslang-greg-front-end.netlify.app/',
     gitUrl: 'https://github.com/greg-front-end/rslang',
-    description: 'Application for learning english. Team work. Graduation project for Rolling Scopes School. Used React, @reduxjs/toolkit, react-router-dom, Typescript.'
+    description:
+      'Application for learning english. Team work. Graduation project for Rolling Scopes School. Used React, @reduxjs/toolkit, react-router-dom, Typescript.',
   },
   {
     name: 'Async race',
     img: './assets/svg/async-race.svg',
     url: 'https://lilithprimary.github.io/rss-stage1-2-tasks/async-race/',
-    gitUrl: 'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/async-race',
-    description: 'Tractor racing. You can create your own tractor or create 100 tractors with different names and colors in one click. Sometimes tractors break down before reaching the finish line (depends of server response). Work with async functions and promises. Used Typescript.'
+    gitUrl:
+      'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/async-race',
+    description:
+      'Tractor racing. You can create your own tractor or create 100 tractors with different names and colors in one click. Sometimes tractors break down before reaching the finish line (depends of server response). Work with async functions and promises. Used Typescript.',
   },
   {
     name: 'Online store',
     img: './assets/svg/online-store.svg',
     url: 'https://lilithprimary.github.io/rss-stage1-2-tasks/online-store/',
-    gitUrl: 'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/online-store',
-    description: 'Fake online store with fruits. Task for working with OOP, TypeScript, MVC-pattern, sort, filter and search.'
+    gitUrl:
+      'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/online-store',
+    description:
+      'Fake online store with fruits. Task for working with OOP, TypeScript, MVC-pattern, sort, filter and search.',
   },
   {
     name: 'News-JS',
     img: './assets/svg/news.svg',
     url: 'https://lilithprimary.github.io/rss-stage1-2-tasks/news-JS/',
     gitUrl: 'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/news-JS',
-    description: 'Migration from JS to typescript. It was working application on javascript. My task was to migrate the application from javascript to typescript with saving full functionality and some upgrades. First meeting with typescript.'
+    description:
+      'Migration from JS to typescript. It was working application on javascript. My task was to migrate the application from javascript to typescript with saving full functionality and some upgrades. First meeting with typescript.',
   },
   {
     name: 'Pseudo Webpack node.js',
     img: './assets/svg/pseudowebpack.svg',
     url: 'https://github.com/LilithPrimary/HTML-builder/tree/main/06-build-page',
-    gitUrl: 'https://github.com/LilithPrimary/HTML-builder/tree/main/06-build-page',
-    description: 'Getting to know the node.js. Сreating a primitive module builder.'
+    gitUrl:
+      'https://github.com/LilithPrimary/HTML-builder/tree/main/06-build-page',
+    description:
+      'Getting to know the node.js. Сreating a primitive module builder.',
   },
   {
     name: 'Pure css slider',
     img: './assets/svg/slider.svg',
     url: 'https://lilithprimary.github.io/cssMemSlider/cssMemSlider/',
     gitUrl: 'https://github.com/LilithPrimary/cssMemSlider/tree/gh-pages',
-    description: 'Need to create slider with jokes on only css, without js, position absolute or something like this.'
+    description:
+      'Need to create slider with jokes on only css, without js, position absolute or something like this.',
   },
   {
     name: 'Virtual keyboard',
     img: './assets/svg/virtual-keyboard.svg',
     url: 'https://lilithprimary.github.io/virtual-keyboard/dist/',
-    gitUrl: 'https://github.com/LilithPrimary/virtual-keyboard/tree/virtual-keyboard',
-    description: 'Need to prevent default all keyboard events and write it by yourselves. First working with webpack. Used javascript.'
+    gitUrl:
+      'https://github.com/LilithPrimary/virtual-keyboard/tree/virtual-keyboard',
+    description:
+      'Need to prevent default all keyboard events and write it by yourselves. First working with webpack. Used javascript.',
   },
   {
     name: 'Shelter',
     img: './assets/svg/shelter.svg',
     url: 'https://rolling-scopes-school.github.io/lilithprimary-JSFE2022Q1/shelter/pages/main/',
-    gitUrl: 'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/gh-pages/shelter',
-    description: 'Layout task for Rolling Scopes School. Pets shelter. Responsive layout, infinity slider, pixel perfet. Used javascript.'
+    gitUrl:
+      'https://github.com/LilithPrimary/rss-stage1-2-tasks/tree/gh-pages/shelter',
+    description:
+      'Layout task for Rolling Scopes School. Pets shelter. Responsive layout, infinity slider, pixel perfet. Used javascript.',
   },
   {
     name: 'Snake',
     img: './assets/svg/snake.svg',
     url: 'https://lilithprimary.github.io/rsschool0/snake/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/snake',
-    description: 'Small random game for Rolling Scopes School. Snake on canvas. Used javascript.'
+    description:
+      'Small random game for Rolling Scopes School. Snake on canvas. Used javascript.',
   },
   {
     name: 'Memory game',
     img: './assets/svg/memory-game.svg',
     url: 'https://lilithprimary.github.io/rsschool0/memory-game/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/memory-game',
-    description: 'Small game for Rolling Scopes School. Memory game.'
+    description: 'Small game for Rolling Scopes School. Memory game.',
   },
   {
     name: 'Tic tac toe',
     img: './assets/svg/tic-tac-toe.svg',
     url: 'https://lilithprimary.github.io/rsschool0/tic-tac-toe/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/tic-tac-toe',
-    description: 'Small game for Rolling Scopes School. Tic-tac-toe. Have some not perfect computer player (but nobody is perfect).'
+    description:
+      'Small game for Rolling Scopes School. Tic-tac-toe. Have some not perfect computer player (but nobody is perfect).',
   },
   {
     name: 'Movie application',
     img: './assets/svg/movie-app.svg',
     url: 'https://lilithprimary.github.io/rsschool0/movie-app/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/movie-app',
-    description: 'Application with info about movies. Old and new ones. Second work with API and promises'
+    description:
+      'Application with info about movies. Old and new ones. Second work with API and promises',
   },
   {
     name: 'Random jokes',
     img: './assets/svg/random-jokes.svg',
     url: 'https://lilithprimary.github.io/rsschool0/random-jokes/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/random-jokes',
-    description: "Quotes from 'Breaking bad' and 'Better call Soul'. First work with API and promises"
+    description:
+      "Quotes from 'Breaking bad' and 'Better call Soul'. First work with API and promises",
   },
   {
     name: 'Audio player',
     img: './assets/svg/audio-player.svg',
     url: 'https://lilithprimary.github.io/rsschool0/audio-player/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/audio-player',
-    description: 'Custom audioplayer. Create my own controls. Used javascript.'
+    description: 'Custom audioplayer. Create my own controls. Used javascript.',
   },
   {
     name: 'Eco sound',
     img: './assets/svg/eco-sound.svg',
     url: 'https://lilithprimary.github.io/rsschool0/eco-sounds/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/eco-sounds',
-    description: 'First work with audio in javascript'
+    description: 'First work with audio in javascript',
   },
   {
     name: "Photographer's portfolio",
     img: './assets/svg/portfolio.svg',
     url: 'https://lilithprimary.github.io/rsschool0/portfolio/',
     gitUrl: 'https://github.com/LilithPrimary/rsschool0/tree/portfolio',
-    description: 'First meeting with html, css, js.'
+    description: 'First meeting with html, css, js.',
   },
-]
+];
 
 export default projects;
